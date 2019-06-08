85141279546fdc4ac17ed5ad87c38e7e3082c837
asantoss <alexsantosantana@live.com>
2019-06-08 13:49:15 -0400

    working through the games

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,6 +1,45 @@
 var content = document.getElementById('content');
 
-var ticTacToe = "Replace this with your own abstraction of Tic Tac Toe"
+var ticTacToe = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+];
+
+var currentTurn = "O";
+
+function cells(board) {
+    let rows = board.map(r => {
+        return r
+    })
+    let cols = rows.map(c => {
+        return c
+    })
+    let myCols = []
+    for (let i = 0; i < rows.length; i++) {
+        document.createElement('div');
+        for (let j = 0; j < cols.length; j++) {
+            columns.innerHTML += `<button row="${board.indexOf(rows[i])}" col="${j}"></button>`
+        }
+        myCols.push(columns)
+    }
+    console.log(myCols)
+    return `<div>${myCols}</div>`
+}
+
+
+function column(row) {
+
+}
+
+function play() {}
+
+
+function winningCondition() {
+
+}
+
+
 
 content.innerHTML = renderGame(ticTacToe);
 
@@ -9,25 +48,10 @@ content.innerHTML = renderGame(ticTacToe);
 
 function renderGame(game) {
     // Change this render function to use the "game" parameter
-
     return `
-        <div class="container d-flex flex-column justify-content-start align-items-center">
-            <h4>It's player O's turn!</h4>
-            <div class="w-50 text-center">
-                <button>X</button>
-                <button>O</button>
-                <button>X</button>
-            </div>
-            <div class="w-50 text-center">
-                <button>-</button>
-                <button>-</button>
-                <button>-</button>
-            </div>
-            <div class="w-50 text-center">
-                <button>O</button>
-                <button>-</button>
-                <button>X</button>
-            </div>
+        <div class="container d-flex flex-column justify-content-start align-items-center" id="board">
+            <h4>It's player ${currentTurn}'s turn!</h4>
+            ${cells(ticTacToe)}
         </div>
     `
 }
